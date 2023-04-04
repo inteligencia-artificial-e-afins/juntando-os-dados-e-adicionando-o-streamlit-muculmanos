dfe3dc32146e835b957845d63ecce90d94e6672c
gabriel buche <bielbuche@gmail.com>
2023-04-04 20:31:21 -0300

    colocado para gerazr planilha excel

--- exercicio.py
+++ exercicio.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 import requests
 
 
@@ -21,6 +22,26 @@ for link in links:
     area =  newSoup.find('div', class_= "col-6").get_text()
     description = newSoup.find_all('p')[3].get_text()
     address = "londrina-PR"
-    aux = [title, value, area, description, address ]
+    aux = [title, value, area.strip(), description, address ]
     myInfoArray.append(aux)
-print(myInfoArray)
+
+workbook = Workbook()
+
+sheet = workbook.active
+
+sheet['A1'] = 'Título'
+sheet['B1'] = 'Endereço'
+sheet['C1'] = 'Aluguel'
+sheet['D1'] = 'Área'
+sheet['E1'] = 'Descrição'
+
+# Adiciona os dados
+for i, row in enumerate(myInfoArray):
+    sheet.cell(row=i+2, column=1, value=row[0])
+    sheet.cell(row=i+2, column=2, value=row[4])
+    sheet.cell(row=i+2, column=3, value=row[1])
+    sheet.cell(row=i+2, column=4, value=row[2])
+    sheet.cell(row=i+2, column=5, value=row[3])
+
+# Salva o arquivo
+workbook.save(filename='scrapImobiliarias2.xlsx')
