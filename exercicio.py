1ab06bfa048966a13f79515cfe86d929067d2688
gabriel buche <bielbuche@gmail.com>
2023-04-05 20:25:54 -0300

    exportação em csv

--- exercicio.py
+++ exercicio.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 import requests
+import pandas as pd
 
-
+import pandas as pd
 url = "https://imobiliariaperez.com.br/alugar" # site para extrair
 response = requests.get(url) #request html
 
@@ -17,12 +18,21 @@ for link in links:
     pushHref = link['href']
     newResponse = requests.get(pushHref)
     newSoup = BeautifulSoup(newResponse.content, 'html.parser') #configurao beautifulsoup
-    title = newSoup.find('h1', class_= "elementor-heading-title").get_text()
-    value = newSoup.find('h2', class_= "elementor-heading-title").get_text()
-    area =  newSoup.find('div', class_= "col-6").get_text()
+    titleA = newSoup.find('h1', class_= "elementor-heading-title").get_text()
+    value = newSoup.find('h2', class_= "elementor-heading-title").get_text() 
     description = newSoup.find_all('p')[3].get_text()
-    address = "londrina-PR"
-    aux = [title, value, area.strip(), description, address ]
+    arrayInfo =  newSoup.find_all('div', class_= "col-6")
+
+    valueArea = arrayInfo[-1].text
+
+    area = valueArea.split()[0] +""+ valueArea.split()[2]
+
+    if value != 'Sob Consulta':
+        value += ",00"
+
+    title = titleA.split()[0]   
+
+    aux = [title, value, area.strip(), description]
     myInfoArray.append(aux)
 
 workbook = Workbook()
@@ -30,18 +40,18 @@ workbook = Workbook()
 sheet = workbook.active
 
 sheet['A1'] = 'Título'
-sheet['B1'] = 'Endereço'
-sheet['C1'] = 'Aluguel'
-sheet['D1'] = 'Área'
-sheet['E1'] = 'Descrição'
+sheet['B1'] = 'Aluguel'
+sheet['C1'] = 'Área'
+sheet['D1'] = 'Descrição'
 
-# Adiciona os dados
 for i, row in enumerate(myInfoArray):
     sheet.cell(row=i+2, column=1, value=row[0])
-    sheet.cell(row=i+2, column=2, value=row[4])
-    sheet.cell(row=i+2, column=3, value=row[1])
-    sheet.cell(row=i+2, column=4, value=row[2])
-    sheet.cell(row=i+2, column=5, value=row[3])
+    sheet.cell(row=i+2, column=2, value=row[1])
+    sheet.cell(row=i+2, column=3, value=row[2])
+    sheet.cell(row=i+2, column=4, value=row[3])
+    
+workbook.save(filename='scrapImobiliaria.xlsx')
 
-# Salva o arquivo
-workbook.save(filename='scrapImobiliarias2.xlsx')
+df = pd.read_excel('scrapImobiliaria.xlsx')
+
+df.to_csv('scrapImobiliaria.csv', index=False)
