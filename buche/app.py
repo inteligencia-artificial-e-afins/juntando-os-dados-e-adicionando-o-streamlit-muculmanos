06f0b8396a7681ee5d30c70e93fdfff604142f6d
gabrielbuche <bielbuche@gmail.com>
2023-05-04 02:36:43 -0300

    juntando dados ateração da pasta

--- buche/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title('Minha primeira aplicação Streamlit')
-    st.write('Bem-vindo(a) à minha aplicação web!')
-
-if __name__ == '__main__':
-    main()
