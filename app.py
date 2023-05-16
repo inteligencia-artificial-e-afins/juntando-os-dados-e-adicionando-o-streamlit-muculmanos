6305c47164781f84838886bbfba574dc42ca23d5
Bruno Vicente <69223732+brunovicentecarvalho@users.noreply.github.com>
2023-05-16 20:40:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from buche.exercicio import pegarDadosBuche
 from lucas.lucas import pegarDadosLucas
+from bruno.bruno import captura_dados
 
 BtnBuche = st.button("Carregar Dados - Buche")
 
@@ -13,4 +14,10 @@ BtnLucas = st.button("Carregar Dados - Lucas")
 
 if BtnLucas:
     data = pegarDadosLucas()
+    st.dataframe(data)
+
+BtnBruno = st.button("Carregar Dados - Bruno")
+
+if BtnBruno:
+    data = captura_dados()
     st.dataframe(data)
